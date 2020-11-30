6a0a2025c9de03b19c226027fe8fddf04fce979b
Bruno Andreghetti Dantas <brunoandreghetti@gmail.com>
2020-11-30 10:36:05 -0300

    Make server loop a tad bit faster

--- main.go
+++ main.go
@@ -22,7 +22,43 @@ func main() {
 
 		time.Sleep(2 * time.Second)
 
-		player(p)
+		serverParams := p.ServerParams()
+		for {
+			sight := p.See()
+			body := p.SenseBody()
+			currentTime := p.Time()
+
+			if sight.Ball == nil {
+				p.Turn(30)
+			} else {
+				ballAngle := sight.Ball.Direction + body.HeadAngle
+				ballDist := sight.Ball.Distance
+				if ballDist < 0.7 {
+					p.Kick(20, 0)
+				} else {
+					p.Dash(50, ballAngle)
+					p.TurnNeck(sight.Ball.Direction)
+				}
+			}
+
+			if p.PlayMode() == "time_over" {
+				p.Bye()
+				break
+			}
+
+			err := p.Error()
+			for err != nil {
+				p.Log(err)
+				err = p.Error()
+			}
+
+			if serverParams.SynchMode {
+				p.DoneSynch()
+				p.WaitSynch()
+			} else {
+				p.WaitNextStep(currentTime)
+			}
+		}
 
 		time.Sleep(2 * time.Second)
 	}
