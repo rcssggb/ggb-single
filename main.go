5baf2db95538902f293c2508a75516ec4bac48c9
Bruno Andreghetti Dantas <brunoandreghetti@gmail.com>
2020-12-09 18:38:44 -0300

    Successfully estimate player body angle

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"time"
 
 	"github.com/rcssggb/ggb-lib/playerclient"
+	"github.com/rcssggb/ggb-lib/rcsscommon"
 	"github.com/rcssggb/ggb-lib/trainerclient"
 )
 
@@ -36,27 +37,57 @@ func main() {
 		serverParams := p.ServerParams()
 		for {
 			sight := p.See()
-			body := p.SenseBody()
+			// body := p.SenseBody()
 			currentTime := p.Time()
 
 			if currentTime == 0 {
 				p.Move(-30, 0)
 			} else {
-				if sight.Ball == nil {
-					p.Turn(30)
-				} else {
-					ballAngle := sight.Ball.Direction + body.HeadAngle
-					ballDist := sight.Ball.Distance
-					if ballDist < 0.7 {
-						p.Kick(20, 0)
-					} else {
-						p.Dash(50, ballAngle)
-						p.TurnNeck(sight.Ball.Direction / 2)
-					}
-				}
+				// if sight.Ball == nil {
+				// 	p.Turn(30)
+				// } else {
+				// 	ballAngle := sight.Ball.Direction + body.HeadAngle
+				// 	ballDist := sight.Ball.Distance
+				// 	if ballDist < 0.7 {
+				// 		p.Kick(20, 0)
+				// 	} else {
+				// 		p.Dash(50, ballAngle)
+				// 		p.TurnNeck(sight.Ball.Direction / 2)
+				// 	}
+				// }
+				p.Turn(10)
 			}
 			pAbsPos := t.GlobalPositions().Teams["single-agent"][1]
-			t.Log(fmt.Sprintf("%f, %f, %f", pAbsPos.X, pAbsPos.Y, pAbsPos.BodyAngle))
+			t.Log(fmt.Sprintf("tAbsolute %f", pAbsPos.BodyAngle))
+
+			var tEstimate float64
+			if sight.Lines.Len() > 0 {
+				closestLine := sight.Lines[0]
+				lDir := closestLine.Direction
+				if lDir < 0 {
+					lDir += 90
+				} else {
+					lDir -= 90
+				}
+				switch closestLine.ID {
+				case rcsscommon.LineRight:
+					tEstimate = 0 - lDir
+				case rcsscommon.LineBottom:
+					tEstimate = 90 - lDir
+				case rcsscommon.LineLeft:
+					tEstimate = 180 - lDir
+				case rcsscommon.LineTop:
+					tEstimate = -90 - lDir
+				}
+			}
+
+			if tEstimate > 180 {
+				tEstimate -= 360
+			} else if tEstimate < -180 {
+				tEstimate += 360
+			}
+
+			p.Log(fmt.Sprintf("tEstimate %f", tEstimate))
 
 			if p.PlayMode() == "time_over" {
 				p.Bye()
@@ -70,7 +101,7 @@ func main() {
 			}
 
 			if currentTime != 0 {
-				time.Sleep(500 * time.Millisecond)
+				time.Sleep(200 * time.Millisecond)
 			}
 
 			if serverParams.SynchMode {
