797d80240056e0f96205e3e03806d78e1dc64423
Samuel <samuel.venzi@me.com>
2021-03-03 06:54:42 -0300

    Refactor scripts to match other branch

--- run.sh
+++ run.sh
@@ -1,38 +1,5 @@
 #!/usr/bin/env bash
 
-HOST_OS=$(uname -s)
-
-echo "Detected OS: ${HOST_OS}"
-
-echo "Exposing X11"
-
-if [ $HOST_OS == "Linux" ]; then
-    xhost +local:root
-elif [ $HOST_OS == "Darwin" ]; then
-    xhost + 127.0.0.1
-else
-    echo "unknown host os"
-    exit
-fi
-
-if [ $HOST_OS == "Linux" ]; then
-    export DISPLAY=${DISPLAY}
-elif [ $HOST_OS == "Darwin" ]; then
-    export DISPLAY="host.docker.internal:0"
-else
-    echo "unknown host os"
-    exit
-fi
-
-docker network create ggb-network
-
-docker-compose up
+docker-compose up rcssserver single-agent
 docker-compose down
 
-echo "Disabling X11 exposure"
-
-if [ $HOST_OS == "Linux" ]; then
-    xhost -local:root
-elif [ $HOST_OS == "Darwin" ]; then
-    xhost - 127.0.0.1
-fi
